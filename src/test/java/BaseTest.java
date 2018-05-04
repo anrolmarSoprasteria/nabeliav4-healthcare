1047465a9ba85beb2327c93d762e17ec03a04c2e
Ángel Roldán Martínez <anrolmar.soprasteria@gmail.com>
2018-05-04 09:44:15 +0200

    Configuring Quartz

---
+++ src/test/java/BaseTest.java
@@ -0,0 +1,9 @@
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@ActiveProfiles({ "test" })
+@RunWith(SpringJUnit4ClassRunner.class)
+public abstract class BaseTest {
+
+}
